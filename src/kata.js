f600be899a9ed587cee256920b5d0995b935456d
Laszlo Feher <Laszlo.Feher@ibm.com>
2022-04-12 10:36:47 +0200

    feat: 2nd, green

--- src/kata.js
+++ src/kata.js
@@ -1,6 +1,11 @@
 class Kata {
 
   tennisGamePoints(score) {
+    let scores = score.split('-');
+    console.log(scores);
+    if (scores[0] === '30') {
+      return 2;
+    }
     return 1;
   }
 }
