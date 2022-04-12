2dcfb8f4a8011497d04268178b8eab78a4e8166f
Laszlo Feher <Laszlo.Feher@ibm.com>
2022-04-12 10:34:55 +0200

    feat: 2nd, red

--- test/kata.test.js
+++ test/kata.test.js
@@ -5,6 +5,11 @@ describe('Checking first scores', () => {
     const kata = new Kata();
     expect(kata.tennisGamePoints('15-0')).toStrictEqual(1);
   });
+
+  test('GIVEN 30-0 WHEN logic THEN returns 2', () => {
+    const kata = new Kata();
+    expect(kata.tennisGamePoints('30-0')).toStrictEqual(2);
+  });
 });
 
 
